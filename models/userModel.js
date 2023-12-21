c4cdcbe7e16b3013eb4068c94380fc0f8ce6ce9b
Hasnain Raza <hasnainraza3899@gmail.com>
2023-12-21 17:23:52 +0500

    create model or schema for user data store

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const User = new mongoose.Model("users", userSchema);
+
+module.exports = User;
