2cfed369bd22495d19768bca7069fddd97923992
Hasnain Raza <hasnainraza3899@gmail.com>
2023-12-24 13:49:42 +0500

    use middleware in register route

--- Router/Auth.js
+++ Router/Auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/Auth")
+const authController = require("../controllers/Auth");
+const signupSchema = require("../validators/authValidation");
+const validate = require("../middlewares/authMiddleware")
 
 
 router.get("/", authController.home)
 
-router.post("/register", authController.register)
+router.post("/register", validate(signupSchema), authController.register)
 router.post("/login", authController.login)
 
 module.exports = router
