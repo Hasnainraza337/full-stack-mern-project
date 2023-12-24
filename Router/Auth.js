8667dbff13af7d76686530068ddf1aa599dc1905
Hasnain Raza <hasnainraza3899@gmail.com>
2023-12-24 12:52:25 +0500

    create login route

--- Router/Auth.js
+++ Router/Auth.js
@@ -6,5 +6,6 @@ const authController = require("../controllers/Auth")
 router.get("/", authController.home)
 
 router.post("/register", authController.register)
+router.post("/login", authController.login)
 
 module.exports = router
