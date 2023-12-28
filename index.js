2872381ada5a001ce312c0e8b37c09456c470ab0
Hasnain Raza <hasnainraza3899@gmail.com>
2023-12-28 14:38:05 +0500

    use cors in app

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config()
 const express = require("express");
+const cors = require("cors")
 const app = express()
 const dbConnect = require("./utils/dbConection")
 const authRouter = require("./Router/Auth");
 const contactRouter = require("./Router/Contact");
 const errorMiddleware = require("./middlewares/errorMiddleware");
+
+const corsoptions = {
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    Credentials: true
+}
+app.use(cors(corsoptions));
+
 app.use(express.json());
-
-
 
 
 app.use("/api/auth", authRouter)
