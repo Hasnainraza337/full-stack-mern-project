c9ce0cecf4453ce96228fa0d44fd2d3c29c1d49a
Hasnain Raza <hasnainraza3899@gmail.com>
2023-12-31 11:02:47 +0500

    create admin middleware and use it in admin route

--- Router/Admin.js
+++ Router/Admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controllers");
 const router = express.Router();
-const authMiddleware = require("../middlewares/userMiddleware")
+const authMiddleware = require("../middlewares/userMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
-router.get("/users", authMiddleware, adminController.getAllUsers);
-router.get("/contacts", authMiddleware, adminController.getAllContacts);
+router.get("/users", authMiddleware, adminMiddleware, adminController.getAllUsers);
+router.get("/contacts", authMiddleware, adminMiddleware, adminController.getAllContacts);
 
 module.exports = router;
